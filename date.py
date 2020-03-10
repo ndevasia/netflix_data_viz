d70ed513421fe83a07e3fd1f621eddbd40b006d1
ejporter <47257372+ejporter@users.noreply.github.com>
2020-03-09 22:39:53 -0400

    new things

--- date.py
+++ date.py
@@ -1,7 +1,7 @@
 import re, datetime
 import pandas as pd
 
-filepath = "C://Users/ndevasia/projects/netflix_data_viz/data/NetflixViewingHistory.csv"
+filepath = "data/NetflixViewingHistory.csv"
 
 day_map = {0: 'Monday', 1: 'Tuesday', 2: 'Wednesday', 3: 'Thursday', 4: 'Friday', 5: 'Saturday', 6: 'Sunday'}
 month_map = {1: 'January', 2: 'February', 3: 'March', 4: 'April', 5: 'May', 6: 'June', 7: 'July', 8: 'August', 9: 'September', 10: 'October', 11: 'November', 12: 'December'}
@@ -38,7 +38,8 @@ for i, j in data.iterrows(): #i is index of row, j.Title = title of show, j.Date
 year_begin = datetime.date(2020, 1, 1)
 today = datetime.date.today()
 days_in_2020 = (today - year_begin).days
+print(year_dict)
 #maybe unhardcode this
-avg_dict = {2018: year_dict[2018]/365, 2019: year_dict[2019]/365, 2020:year_dict[2020]/days_in_2020}
+avg_dict = {2018: year_dict[18]/365, 2019: year_dict[19]/365, 2020:year_dict[20]/days_in_2020}
 data["Day of Week"] = days_of_week
 data.to_csv("data/NetflixViewingHistory.csv", index = False)
